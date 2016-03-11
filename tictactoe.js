602ae10faac369aa9e8d633978a2e41023041247
Jay Byun <jaybyun@Macintosh.local>
2016-03-11 14:14:02 +1100

    finalized

--- tictactoe.js
+++ tictactoe.js
@@ -15,88 +15,14 @@ var win7 = [1,5,9];
 var win8 = [3,5,7];
 var wins = [win1, win2, win3, win4, win5, win6, win7, win8]
 
-// Detects whether game has finished
-
-// var gameEnd = false;
-// var checkIfGameEnd = function(){
-//   if (($(".sq1").text() && $(".sq2").text() && $(".sq3").text() !== ("")) &&
-//   ($(".sq4").text() && $(".sq5").text() && $(".sq6").text() !== ("")) &&
-//   ($(".sq7").text() && $(".sq8").text() && $(".sq9").text() !== ("")))
-// {
-//   gameEnd = true;
-//   checkWin();
-//   winAlert();
-// }
-// }
-
 var turns = 0;
 var gameEnd = function (){
   turns++;
+  console.log(turns, winsX, winsO);
   if ((turns === 9) && (winsX === false) && (winsO === false)){
     alert("Its a Tie.")
   }
 };
-//
-// var turns = 0;
-// var gameEnd = function (){
-//   turns++;
-//   if ((turns === 9) && (winsX === false) && (winsO === false)){
-//     alert(turns)
-//   }
-// };
-
-// Check for Winner
-
-// var winX = 0;
-// var winO = 0;
-// var tie = 0;
-// checkWin = function () {
-    // if (($(".sq1") === $(".sq2") && $(".sq1") === $(".sq3") && ($(".sq1").text() === ("X"))) || //first row
-    // ($(".sq4") === $(".sq5") && $(".sq4") === $(".sq6") && ($(".sq4").text() === ("X"))) || //second row
-    // ($(".sq7") === $(".sq8") && $(".sq7") === $(".sq9") && ($(".sq7").text() === ("X"))) || //third row
-    // ($(".sq1") === $(".sq4") && $(".sq1") === $(".sq7") && ($(".sq1").text() === ("X"))) || //first column
-    // ($(".sq2") === $(".sq5") && $(".sq2") === $(".sq8") && ($(".sq2").text() === ("X"))) || //second column
-    // ($(".sq3") === $(".sq6") && $(".sq3") === $(".sq9") && ($(".sq3").text() === ("X"))) || //third column
-    // ($(".sq1") === $(".sq5") && $(".sq1") === $(".sq9") && ($(".sq1").text() === ("X"))) || //diagonal 1
-    // ($(".sq3") === $(".sq5") && $(".sq3") === $(".sq7") && ($(".sq3").text() === ("X")))) //diagonal 2
-    // {
-    // if ($(".boardTop").text() === "X" || $(".boardMid").text() === "X" || $(".boardBot").text() === "X")
-    // $(".sq1") === $(".sq4") && $(".sq1") === $(".sq7") && $(".sq1").text() === "X") || //first column
-    // $(".sq2") === $(".sq5") && $(".sq2") === $(".sq8") && $(".sq2").text() === "X") || //second column
-    // $(".sq3") === $(".sq6") && $(".sq3") === $(".sq9") && $(".sq3").text() === "X") || //third column
-    // $(".sq1") === $(".sq5") && $(".sq1") === $(".sq9") && $(".sq1").text() === "X") || //diagonal 1
-    // $(".sq3") === $(".sq5") && $(".sq3") === $(".sq7") && $(".sq3").text() === "X")) //diagonal 2
-      // console.log("Finished Game!")
-      // alert("X Wins!");
-// }
-      // winX = true;
-      // winAlert();
-    //  else if {
-    // (($(".sq1") === $(".sq2") && $(".sq1") === $(".sq3") && ($(".sq1").text() === ("O"))) || //first row
-    // ($(".sq4") === $(".sq5") && $(".sq4") === $(".sq6") && ($(".sq4").text() === ("O"))) || //second row
-    // ($(".sq7") === $(".sq8") && $(".sq7") === $(".sq9") && ($(".sq7").text() === ("O"))) || //third row
-    // ($(".sq1") === $(".sq4") && $(".sq1") === $(".sq7") && ($(".sq1").text() === ("O"))) || //first column
-    // ($(".sq2") === $(".sq5") && $(".sq2") === $(".sq8") && ($(".sq2").text() === ("O"))) || //second column
-    // ($(".sq3") === $(".sq6") && $(".sq3") === $(".sq9") && ($(".sq3").text() === ("O"))) || //third column
-    // ($(".sq1") === $(".sq5") && $(".sq1") === $(".sq9") && ($(".sq1").text() === ("O"))) || //diagonal 1
-    // ($(".sq3") === $(".sq5") && $(".sq3") === $(".sq7") && ($(".sq3").text() === ("O")))) //diagonal 2
-    //  {
-    // ($(".boardTop").each.text() === "O" || $(".boardMid").each.text() === "O" || $(".boardBot").each.text() === "O")
-    // $(".sq1") === $(".sq4") && $(".sq1") === $(".sq7") && $(".sq1").text() === "O") || //first column
-    // $(".sq2") === $(".sq5") && $(".sq2") === $(".sq8") && $(".sq2").text() === "O") || //second column
-    // $(".sq3") === $(".sq6") && $(".sq3") === $(".sq9") && $(".sq3").text() === "O") || //third column
-    // $(".sq1") === $(".sq5") && $(".sq1") === $(".sq9") && $(".sq1").text() === "O") || //diagonal 1
-    // $(".sq3") === $(".sq5") && $(".sq3") === $(".sq7") && $(".sq3").text() === "O")) //diagonal 2
-      //  console.log("Finished Game")
-      //  alert("O Wins!");
-  // }
-      // winO = true;
-      //   winAlert();
-  //      else {
-  //         tie = true;
-  //         alert("It's a Draw!");
-  //   }
-  // }
 
 var checkWin = function() {
   gameEnd();
@@ -126,8 +52,6 @@ var checkWin = function() {
     }
   };
 
-//Win Counter
-
 winCounter = function() {
    if (winX === true) {
      winsX++
@@ -137,87 +61,6 @@ winCounter = function() {
      $('.results').append("div.O");
    }
  }
-
-// Place X or O on the board
-
-// var playerMove = function(){
-//       turn = turn ? false : true;
-//       console.log("turn:"+ turn);
-//       checkWin();
-//       this.playerMove = playerMove;
-//     }
-
-// $("div.sq2").on("click", function(){
-//   if ($("div.sq2").text() === ("")){
-//     $(this).text("X");
-//   } else if ($("div.sq2").text() === ("")){
-//     $(this).text("O");
-//   } else
-//   alert("Please Choose Another Square");
-// })
-//
-// $("div.sq3").on("click", function(){
-//   if ($("div.sq3").text() === ("")){
-//     $(this).text("X");
-//   } else if ($("div.sq3").text() === ("")){
-//     $(this).text("O");
-//   } else
-//   alert("Please Choose Another Square");
-// })
-//
-// $("div.sq4").on("click", function(){
-//   if ($("div.sq4").text() === ("")){
-//     $(this).text("X");
-//   } else if ($("div.sq4").text() === ("")){
-//     $(this).text("O");
-//   } else
-//   alert("Please Choose Another Square");
-// })
-//
-// $("div.sq5").on("click", function(){
-//   if ($("div.sq5").text() === ("")){
-//     $(this).text("X");
-//   } else if ($("div.sq5").text() === ("")){
-//     $(this).text("O");
-//   } else
-//   alert("Please Choose Another Square");
-// })
-//
-// $("div.sq6").on("click", function(){
-//   if ($("div.sq6").text() === ("")){
-//     $(this).text("X");
-//   } else if ($("div.sq6").text() === ("")){
-//     $(this).text("O");
-//   } else
-//   alert("Please Choose Another Square");
-// })
-//
-// $("div.sq7").on("click", function(){
-//   if ($("div.sq7").text() === ("")){
-//     $(this).text("X");
-//   } else if ($("div.sq7").text() === ("")){
-//     $(this).text("O");
-//   } else
-//   alert("Please Choose Another Square");
-// })
-//
-// $("div.sq8").on("click", function(){
-//   if ($("div.sq8").text() === ("")){
-//     $(this).text("X");
-//   } else if ($("div.sq8").text() === ("")){
-//     $(this).text("O");
-//   } else
-//   alert("Please Choose Another Square");
-// })
-//
-// $("div.sq9").on("click", function(){
-//   if ($("div.sq9").text() === ("")){
-//     $(this).text("X");
-//   } else if ($("div.sq9").text() === ("")){
-//     $(this).text("O");
-//   } else
-//   alert("Please Choose Another Square");
-// }
 
 var currentPlayer = 'X';
 $(".square").on("click", function() {
@@ -231,9 +74,6 @@ $(".square").on("click", function() {
   }
   checkWin();
 });
-
-
-// Alerts if win
 
 winAlert = function(){
   if (winX === true) {
